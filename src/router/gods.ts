5fb29700b603d7a24ed48a1641d46234bf947016
Sam <s.wilson@echosec.net>
2023-07-22 08:47:15 -0700

    Update routes

--- src/router/gods.ts
+++ src/router/gods.ts
@@ -1,9 +1,11 @@
 import type { RouteRecordRaw } from "vue-router";
 import agorus from '@/data/gods/agorus/data';
+import enoreth from '@/data/gods/enoreth/data';
 import surad from '@/data/gods/surad/data';
 import po from '@/data/gods/po/data';
 import thal from '@/data/gods/thal/data';
 import ulat from '@/data/gods/ulat/data';
+import ura from '@/data/gods/ura/data';
 import { Routes } from '@/constants/routes';
 import { Gods } from '@/constants/gods';
 
@@ -19,12 +21,32 @@ export default [
     component: () => import('../views/GodsView.vue'),
   },
   {
+    path: `/${Routes.GODS}/${Gods.URA}`,
+    props: { activeGod: ura },
+    component: () => import('../views/GodsView.vue'),
+  },
+  {
     path: `/${Routes.GODS}/${Gods.THAL}`,
     props: { activeGod: thal },
     component: () => import('../views/GodsView.vue'),
   },
   {
+    path: `/${Routes.GODS}/${Gods.ENORETH}`,
+    props: { activeGod: enoreth },
+    component: () => import('../views/GodsView.vue'),
+  },
+  {
     path: `/${Routes.GODS}/${Gods.PO}`,
+    props: { activeGod: po },
+    component: () => import('../views/GodsView.vue'),
+  },
+  {
+    path: `/${Routes.GODS}/${Gods.ETH_KOREL}`,
+    props: { activeGod: po },
+    component: () => import('../views/GodsView.vue'),
+  },
+  {
+    path: `/${Routes.GODS}/${Gods.MALAK}`,
     props: { activeGod: po },
     component: () => import('../views/GodsView.vue'),
   },
